35a1fe83efc0d4dbb2df3157e84dfc989388b802
Yuhanas Yulianto <yulianto.yuhanas@gmail.com>
2018-12-22 21:07:24 +0700

    install react and react-dom, and test the babel-preset-react

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return(
+    <div>
+      <h1>Are you a good React(ion)?</h1>
+    </div>
+  )
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log('test my webpack configuration ==> it is working');
+import React from 'react';
+import { render } from 'react-dom';
+
+import App from './App';
+
+render(<App />, document.getElementById('root'));
