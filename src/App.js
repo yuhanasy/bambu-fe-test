6196e53682813de1073f79645be4fc10cf83273e
Yuhanas Yulianto <yulianto.yuhanas@gmail.com>
2018-12-22 21:39:00 +0700

    modify scripts, change target file to default main.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ const App = () => {
   return(
     <div>
       <h1>Are you a good React(ion)?</h1>
+      <p>plese give an awesome react(ion)</p>
+      <p>test add new line</p>
     </div>
   )
 }
