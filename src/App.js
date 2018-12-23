c494c67219562fef2915049e421153dd2268581d
Yuhanas Yulianto <yulianto.yuhanas@gmail.com>
2018-12-23 11:14:31 +0700

    install react-hot-loader to enable auto refresh in browser

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const App = () => {
       <h1>Are you a good React(ion)?</h1>
       <p>plese give an awesome react(ion)</p>
       <p>test add new line</p>
-      <p>is it refresh automagically?</p>
+      <p>is it refresh <strong>automagically?</strong></p>
     </div>
   )
 }
