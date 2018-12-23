23590f13be38497a0de517beb65cc75b52b73410
Yuhanas Yulianto <yulianto.yuhanas@gmail.com>
2018-12-23 10:46:58 +0700

    solved error [WDS] disconnected by downgrade the WDS into 3.1.10 version

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ const App = () => {
       <h1>Are you a good React(ion)?</h1>
       <p>plese give an awesome react(ion)</p>
       <p>test add new line</p>
+      <p>is it refresh automagically?</p>
     </div>
   )
 }
