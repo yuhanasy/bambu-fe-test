9658d670b1ecbac99d25fff0a0c217110db179e5
Yuhanas Yulianto <yulianto.yuhanas@gmail.com>
2018-12-25 16:19:50 +0700

    add sidebar card

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -3,13 +3,22 @@ import React, { Component } from "react";
 import { SidebarWrapper, SidebarCardGroup, SidebarCard } from "./styles";
 
 class SideBar extends Component {
+  constructor(props) {
+    super(props)
+  }
+
   render() {
+    const symbols = this.props.symbols;
     return (
       <SidebarWrapper>
         <SidebarCardGroup>
-          <SidebarCard>
-            AAPL
-          </SidebarCard>
+          {symbols.map(symbol => {
+            return(
+              <SidebarCard key={symbol}>
+                {symbol}
+              </SidebarCard>
+            )
+          })}
         </SidebarCardGroup>
       </SidebarWrapper>
     )
