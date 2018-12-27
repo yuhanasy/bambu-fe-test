b0aaf6687a06ad2f842c13306c923ac1cf60d506
Yuhanas Yulianto <yulianto.yuhanas@gmail.com>
2018-12-27 08:53:19 +0700

    add X and Y axis

--- src/components/chart/index.js
+++ src/components/chart/index.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 
-import { ChartContainer, Chart } from './styles';
+import { ChartContainer, Chart, Axes } from './styles';
 import { data } from './data';
 
 const timeSeries = data["Time Series (Daily)"];
 
-let arr = Object.values(timeSeries);
-let keys = Object.keys(timeSeries);
+let arr = Object.values(timeSeries).reverse();
+console.log(arr);
 
 let flattenData = array => {
   const output = [];
@@ -23,33 +23,30 @@ let flattenData = array => {
 const arrayOfData = flattenData(arr);
 
 let min = Math.min(...arrayOfData);
-
 let max = Math.max(...arrayOfData);
 
 let gridMin = Math.round(min/5) * 5 - 2;
 let gridMax = Math.round(max/5) * 5 + 2;
-    
+
 const dataRange = gridMax - gridMin;
-let yGrids = new Array(dataRange);
 const dataLength = arr.length;
+
+let yGrids = Array.from(new Array(dataRange),(val,index)=>index);
+let xGrids = Object.keys(timeSeries).reverse();
 
 
 class ChartWrapper extends Component {
   render() {
     const width = 300;
     const height = 150;
-    let intervalX = width / dataLength;
-    let intervalY = height / dataRange;
+    const intervalX = width / dataLength;
+    const intervalY = height / dataRange;
+    let month = '01';
+    const months = {'01': 'Jan', '02': 'Feb', '03': 'Mar', '04': 'Apr', '05': 'May', '06': 'Jun', '07': 'Jul', '08': 'Aug', '09': 'Sep', '10': 'Oct', '11': 'Nov', '12': 'Dec'};
     const coordinateY = (y) => height - (y - gridMin) * intervalY;
     return(
       <ChartContainer>
         <Chart viewBox={`0 0 ${width} ${height}`} preserveAspectRatio={"none"}>
-        <g id='yGrids'>
-          {
-
-          }
-        </g>
-
         <g id='ohlc'>
           {arr.map((data, index)=> {
             let x = (index * intervalX) + intervalX;
@@ -69,9 +66,37 @@ class ChartWrapper extends Component {
           })}
         </g>
         </Chart>
+        <Axes viewBox={`0 0 ${width} ${height}`} preserveAspectRatio={"none"}>
+          <g>
+            <line x1='10%' x2='10%' y1='0' y2='90%' stroke='#000' strokeWidth='.5' />
+            <line x1='10%' x2 ='100%' y1='90%' y2='90%' stroke='#000' strokeWidth='.5' />
+            {yGrids.map(grid => {
+              let tick = (height - (grid * intervalY * 0.9)) - (height * 0.1);
+              let tickMark = gridMin + grid;
+              return (
+                <g key={grid} >
+                  <line x1='9%' x2='10%' y1={tick} y2={tick} stroke='#000' strokeWidth='.5' />
+                  {tickMark % 5 === 0 ? (<text x='10' y={tick} textLength='10'>$ {tickMark}</text>) : (null)}
+                  
+                </g>
+              )
+            })}
+            {xGrids.map((tickMark, index, arr) => {
+              let tick = ((width / arr.length) * index * 0.9) + (width * 0.1);
+                if (tickMark.substr(5,2) !== month) {
+                  month = tickMark.substr(5,2);
+                  return (
+                  <g key={tick}>
+                    <line x1={tick} x2={tick} y1='90%' y2='91%' stroke='#000' strokeWidth='.5' />
+                    <text x={tick + 20} y='92%'>{months[month]}</text>
+                  </g>
+                  )
+                }
+            })}
+          </g>
+        </Axes>
       </ChartContainer>
     )
   }
 }
-
 export default ChartWrapper;
--- src/components/chart/styles.js
+++ src/components/chart/styles.js
@@ -3,11 +3,22 @@ import styled from "@emotion/styled/";
 export const ChartContainer = styled.div`
   width: 100%;
   height: 100%;
+  position: relative;
 `;
 
 export const Chart = styled.svg`
-  width: 100%;
+  width: 90%;
   min-width: 600px;
-  height: 100%;
+  height: 90%;
+  position: absolute;
+  left: 10%;
   background-color: #FFBF69;
+`;
+
+export const Axes = styled.svg`
+  width: 100%;
+  height: 100%;
+  font-size: 4px;
+  position:absolute;
+  z-index: 5;
 `;
