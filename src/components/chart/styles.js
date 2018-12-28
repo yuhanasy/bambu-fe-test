483b32e6fe67d783ecc1d4faed366d58b281a7ea
Yuhanas Yulianto <yulianto.yuhanas@gmail.com>
2018-12-28 17:14:37 +0700

    add title chart and fix overflow chart

--- src/components/chart/styles.js
+++ src/components/chart/styles.js
@@ -1,5 +1,7 @@
 import styled from "@emotion/styled/";
 
+const chartWidth = 600;
+
 export const ChartContainer = styled.div`
   width: 100%;
   height: 100%;
@@ -7,8 +9,18 @@ export const ChartContainer = styled.div`
 
   @media (max-width: 450px) {
     overflow-x: scroll;
+    ::-webkit-scrollbar {
+      display: none;
+    }
   }
-  `;
+`;
+
+export const ChartTitle = styled.h3`
+  color: #fff;
+  font-weight: 500;
+  text-align: center;
+  margin: 0 auto;
+`;
 
 export const Chart = styled.svg`
   width: 90%;
@@ -17,9 +29,10 @@ export const Chart = styled.svg`
   left: 10%;
 
   @media (max-width: 450px) {
-  width: 120%;
+    width: ${chartWidth * 0.9}px;
+    left: ${chartWidth * 0.1}px;
   }
-  `;
+`;
 
 export const Axes = styled.svg`
   width: 100%;
@@ -30,6 +43,6 @@ export const Axes = styled.svg`
   z-index: 5;
 
   @media (max-width: 450px) {
-  width: 130%;
+    width: ${chartWidth}px;
   }
 `;
